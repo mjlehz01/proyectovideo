40728b5918bef34a40454938ede7c543aaa15a25
Jesus Hernandez <mjlehz@gmail.com>
2020-09-08 22:35:57 -0500

    VersionRoutes

---
+++ src/components/Layout.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+
+const Layout = ({ children }) => (
+  <div className="app">
+    <Header />
+    {children}
+    <Footer />
+  </div>
+);
+export default Layout;
