65fa967edbcff25e20c46583dd7202e2775832cd
Jesus Hernandez <mjlehz@gmail.com>
2020-09-08 23:02:57 -0500

    version0.3

---
+++ README.md
@@ -0,0 +1,5 @@
+
+
+
+
+version0.1: creado solo un reducer sin problemas
---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { Redirect } from "react-router-dom"
+
+const reducer =(state,action)=>{
+    return state
+}
+export default reducer
